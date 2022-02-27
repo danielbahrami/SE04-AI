8f4cfc7400cfaf920d95615107afc8cf023a591e
Daniel Bahrami <daniel@bahrami.dk>
2022-02-27 23:54:13 +0100

    Table Driven Agent

---
+++ Lab 1/Exercises/table_driven_agent.py
@@ -0,0 +1,32 @@
+A = 'A'
+B = 'B'
+percepts = []
+table = {
+    ((A, 'Clean'),): 'Right',
+    ((A, 'Dirty'),): 'Suck',
+    ((B, 'Clean'),): 'Left',
+    ((B, 'Dirty'),): 'Suck',
+    ((A, 'Clean'), (A, 'Clean')): 'Right',
+    ((A, 'Clean'), (A, 'Dirty')): 'Suck',
+    ((A, 'Clean'), (A, 'Clean'), (A, 'Clean')): 'Right',
+    ((A, 'Clean'), (A, 'Clean'), (A, 'Dirty')): 'Suck',
+    ((A, 'Clean'), (A, 'Dirty'), (B, 'Clean')): 'Left'
+}
+
+def LOOKUP(percepts, table):
+    action = table.get(tuple(percepts))
+    return action
+
+def TABLE_DRIVEN_AGENT(percept):
+    percepts.append(percept)
+    action = LOOKUP(percepts, table)
+    return action
+
+def run():
+    print('Action\tPercepts')
+    print(TABLE_DRIVEN_AGENT((A, 'Clean')), '\t', percepts)
+    print(TABLE_DRIVEN_AGENT((A, 'Dirty')), '\t', percepts)
+    print(TABLE_DRIVEN_AGENT((B, 'Clean')), '\t', percepts)
+
+if __name__ == '__main__':
+    run()
